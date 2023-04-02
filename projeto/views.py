fcef935daf58c8c40f4ae2a2b0f9af536495675c
MatheusMorata <matlucar09@hotmail.com>
2023-04-02 16:24:15 -0300

    Alteracoes view e template

--- projeto/views.py
+++ projeto/views.py
@@ -8,9 +8,9 @@ def cotacao(data):
     resultado = r.get(url) #Método GET
     json = resultado.json()
 
-    EUR = 1/float(json["rates"]["USD"])
-    BRL = float(json["rates"]["BRL"])/float(json["rates"]["USD"])
-    JPY = float(json["rates"]["JPY"])/float(json["rates"]["USD"])
+    EUR = 1/float(json['rates']['USD'])
+    BRL = float(json['rates']['BRL'])/float(json['rates']['USD'])
+    JPY = float(json['rates']['JPY'])/float(json['rates']['USD'])
 
     json = {'EUR':EUR,
             'BRL':BRL,
@@ -24,18 +24,21 @@ def home(request):
     valores_euro = []
     valores_real = []
     valores_iene = []
+    d = []
 
     for i in range(0,5):
         dia = data_hoje.day - i
         if (dia >= 1) and (dia <= 31):
             data = str(data_hoje.year) + "-" + str(data_hoje.month) + "-" + str(dia) #Aqui ele formata a data que vai ser usada na requisição
-            print(dia)
+            d.append(data)
             valores_euro.append(round(cotacao(data)['EUR'],2))
             valores_real.append(round(cotacao(data)['BRL'],2))
             valores_iene.append(round(cotacao(data)['JPY'],2))
 
-    c = {"EUR":valores_euro,
-        "BRL":valores_real,
-        "JPY":valores_iene}
+
+    c = {'EUR':valores_euro,
+        'BRL':valores_real,
+        'JPY':valores_iene,
+        'datas':d}
 
     return render(request,"home.html",c)
